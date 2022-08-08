90873f831e95312d5eaa33c0a3cc39c26f286e38
Carlos Alberto García de Alba Chávez <carlos.alberto.gdea.ch@gmail.com>
2022-08-08 17:35:33 -0500

    Finished Responsive Design
    Finished the Responsive Design for the FAQ Section

--- src/components/Pages/FAQ/FAQ.jsx
+++ src/components/Pages/FAQ/FAQ.jsx
@@ -6,48 +6,69 @@ import "./index.css"
 
 
 function FAQ() {
-    return (
-        <>
-        <section className='FAQSection'>
-        <h2 className='faq-title'>Preguntas Frecuentes</h2>
-        <Accordion defaultActiveKey={['0']} alwaysOpen className='accordion-flush'>
-        <div className='flex-left'>
-        <Accordion.Item eventKey="0">
-        <Accordion.Header>¿Lorem Ipsum dolor sit amet?</Accordion.Header>
-        <Accordion.Body>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vehicula lacinia odio. Etiam sem turpis, egestas ut eros commodo, laoreet porttitor velit. 
-        </Accordion.Body>
-        </Accordion.Item>
-        <Accordion.Item eventKey="1">
-        <Accordion.Header>¿Lorem Ipsum dolor sit amet?</Accordion.Header>
-        <Accordion.Body>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vehicula lacinia odio. Etiam sem turpis, egestas ut eros commodo, laoreet porttitor velit.
-          <button className='faq-button'>Lorem Ipsum</button>
-        </Accordion.Body>
-        </Accordion.Item>
+  return (
+    <>
+      <section className='FAQSection content'>
+        <div class="row">
+          <h2 className='faq-title display-2'>Preguntas Frecuentes</h2>
         </div>
-        <div className='flex-right'>
-        <Accordion.Item eventKey="2">
-        <Accordion.Header>¿Lorem Ipsum dolor sit amet</Accordion.Header>
-        <Accordion.Body>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vehicula lacinia odio. Etiam sem turpis, egestas ut eros commodo, laoreet porttitor velit.
-        </Accordion.Body>
+        <Accordion defaultActiveKey={['0']} alwaysOpen className='row accordion-flush'>
+          <div className='col-12 col-md-6 col-sm-12 col-xs-12'>
+            <Accordion.Item eventKey="0">
+              <Accordion.Header >¿Lorem Ipsum dolor sit amet?</Accordion.Header>
+              <Accordion.Body className="h3">
+                <div className='row'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vehicula lacinia odio. Etiam sem turpis, egestas ut eros commodo, laoreet porttitor velit.
+                </div>
+                <div className='row'>
+
+                </div>
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="1">
+              <Accordion.Header>¿Lorem Ipsum dolor sit amet?</Accordion.Header>
+              <Accordion.Body className="h3">
+                <div className='row'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vehicula lacinia odio. Etiam sem turpis, egestas ut eros commodo, laoreet porttitor velit.
+                </div>
+
+                <div className='row'>
+                  <button className="faq-button col-4">Lorem Ipsum</button>
+                </div>
+              </Accordion.Body>
+            </Accordion.Item>
+          </div>
+          <div className='col-12 col-md-6 col-sm-12 col-xs-12'>
+            <Accordion.Item eventKey="2">
+              <Accordion.Header>¿Lorem Ipsum dolor sit amet</Accordion.Header>
+              <Accordion.Body className="h3">
+                <div className='row'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vehicula lacinia odio. Etiam sem turpis, egestas ut eros commodo, laoreet porttitor velit.
+                </div>
+                
+                <div className='row mt-3'>
+                </div></Accordion.Body>
+
+            </Accordion.Item>
+            <Accordion.Item eventKey="3">
+              <Accordion.Header>¿Lorem Ipsum dolor sit amet</Accordion.Header>
+              <Accordion.Body className="h3">
+              <div className='row'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vehicula lacinia odio. Etiam sem turpis, egestas ut eros commodo, laoreet porttitor velit.
+                </div>
+                
+                <div className="row mt-3">
+                  <a className="faq-link col-6">Lorem Ipsum</a>
+                </div></Accordion.Body>
+            </Accordion.Item>
+          </div>
+
+        </Accordion>
         
-      </Accordion.Item>
-      <Accordion.Item eventKey="3">
-        <Accordion.Header>¿Lorem Ipsum dolor sit amet</Accordion.Header>
-        <Accordion.Body>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vehicula lacinia odio. Etiam sem turpis, egestas ut eros commodo, laoreet porttitor velit.
-        <a href="https://google.com.mx" className='faq-link'>Lorem ipsum</a>
-        </Accordion.Body>
-      </Accordion.Item>
-        </div>
-      
-    </Accordion>       
-        </section>
-          
-        </>
-    )
+      </section>
+
+    </>
+  )
 }
 
 export default FAQ;
