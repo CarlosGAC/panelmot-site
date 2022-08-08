09307ffb843f38cbca89685c30e60b089a1c8cda
Carlos Alberto García de Alba Chávez <carlos.alberto.gdea.ch@gmail.com>
2022-08-08 18:20:59 -0500

    Responsive Design ready
    Fixed issue with the responsiveness at the Contact Data.
    Added the Copyright and Privacy Notice at the bottom of the page

--- src/components/Pages/Contact/Contact.jsx
+++ src/components/Pages/Contact/Contact.jsx
@@ -12,7 +12,7 @@ function FAQ() {
         <>
             <section className='ContactSection'>
                 <div className="container-fluid">
-                    <div className="row">
+                    <div className="row mb-5">
                         <div className="col-xl-8 col-md-6 col-sm-12 col-xs-12">
                             <h2 className="section-title display-2">Contáctanos</h2>
                             <p className="section-description h4 mt-4 mb-5">Si te interesan nuestros productos, no dudes en contactarnos!</p>
@@ -40,30 +40,54 @@ function FAQ() {
                             <div className="contact-data-container">
 
                                 <div className='row contact-row mb-2 col-12'>
-                                    <div className='col-12 col-lg-12'>
-                                        <p className="h2"><i><BsTelephone /></i><b>342 108 4567</b></p>
+                                    <div className='col-2'>
+                                    <i><BsTelephone /></i>
+                                    </div>
+                                    <div className='col-10'>
+                                        <p className="h2"><b>342 108 4567</b></p>
                                     </div>
                                 </div>
 
                                 <div className='row contact-row mb-2 col-12'>
-                                    <div className="col-12 col-lg-12">
-                                        <i><GoMail /></i><a className="contact-link h2" href="mailto:panelpvcmot@gmail.com"><b>panelpvcmot@gmail.com</b></a>
+                                <div className='col-2'>
+                                    <i><GoMail /></i>
+                                    </div>
+                                    <div className='col-10'>
+                                    <a className="contact-link h2" href="mailto:panelpvcmot@gmail.com"><b>panelpvcmot@gmail.com</b></a>
                                     </div>
                                 </div>
 
                                 <div className='row contact-row mb-2 col-12'>
-                                    <div className='col-12 col-lg-12'>
-                                        <i><FaFacebookSquare /></i>
-                                        <a className="contact-link h2" href="https://www.facebook.com/PANEL-PVC-172312544911856"><b><u>Panel PVC</u></b></a>
+                                <div className='col-2'>
+                                <i><FaFacebookSquare /></i>
+                                    </div>
+                                    <div className='col-10'>
+                                    <a className="contact-link h2" href="https://www.facebook.com/PANEL-PVC-172312544911856"><b><u>Panel PVC</u></b></a>
                                     </div>
                                 </div>
 
                                 <div className='row contact-row mb-2 col-12'>
+                                <div className='col-2'>
+                                    <i><HiOutlineLocationMarker /></i>
+                                </div>
+                                <div className='col-10'>
+                                    <p className="h2"><b>Guadalajara, Jalisco</b></p>
+                                </div>
                                     <div className='col-12 col-lg-12'>
-                                        <p className="h2"><i><HiOutlineLocationMarker /></i><b>Guadalajara, Jalisco</b></p>
                                     </div>
                                 </div>
                             </div>
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <div className='col-5'>
+                            <p className='h4'>Página Web desarrollada por <a href="https://eldritchgames.com.mx">Eldritch Games</a></p>
+                        </div>
+                        <div className='col-3'>
+                            <a href="#" className='h4'>Aviso de Privacidad</a>
+                        </div>
+                        <div className='col-4'>
+                            <p className='h4'><u>Copyright © 2021 by Panelmot PVC</u></p>
                         </div>
                     </div>
                 </div>
