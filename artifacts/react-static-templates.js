972901238f85a51873ddc6a95c2b489b0bfa9a5a
Carlos Alberto García de Alba Chávez <carlos.alberto.gdea.ch@gmail.com>
2022-08-16 16:22:23 -0500

    Moved all Pages to the Organisms folder

--- artifacts/react-static-templates.js
+++ artifacts/react-static-templates.js
@@ -4,17 +4,7 @@
 // Template Map
 export default {
   '__react_static_root__/node_modules/react-static/lib/browser/components/Default404': require('__react_static_root__/node_modules/react-static/lib/browser/components/Default404').default,
-'__react_static_root__/src/components/Pages/About/About.jsx': require('__react_static_root__/src/components/Pages/About/About.jsx').default,
-'__react_static_root__/src/components/Pages/About/index.js': require('__react_static_root__/src/components/Pages/About/index.js').default,
-'__react_static_root__/src/components/Pages/AboutUs/AboutUs.jsx': require('__react_static_root__/src/components/Pages/AboutUs/AboutUs.jsx').default,
-'__react_static_root__/src/components/Pages/AboutUs/index.js': require('__react_static_root__/src/components/Pages/AboutUs/index.js').default,
-'__react_static_root__/src/components/Pages/Blog/Blog.jsx': require('__react_static_root__/src/components/Pages/Blog/Blog.jsx').default,
-'__react_static_root__/src/components/Pages/Blog/index.js': require('__react_static_root__/src/components/Pages/Blog/index.js').default,
-'__react_static_root__/src/components/Pages/Contact/Contact.jsx': require('__react_static_root__/src/components/Pages/Contact/Contact.jsx').default,
-'__react_static_root__/src/components/Pages/Contact/index.js': require('__react_static_root__/src/components/Pages/Contact/index.js').default,
 '__react_static_root__/src/components/Pages/index.js': require('__react_static_root__/src/components/Pages/index.js').default,
-'__react_static_root__/src/components/Pages/Index/index.js': require('__react_static_root__/src/components/Pages/Index/index.js').default,
-'__react_static_root__/src/components/Pages/Index/Welcome.jsx': require('__react_static_root__/src/components/Pages/Index/Welcome.jsx').default,
 '__react_static_root__/src/components/Pages/NotFound/index.js': require('__react_static_root__/src/components/Pages/NotFound/index.js').default,
 '__react_static_root__/src/components/Pages/NotFound/NotFound.jsx': require('__react_static_root__/src/components/Pages/NotFound/NotFound.jsx').default,
 '__react_static_root__/src/containers/Post': require('__react_static_root__/src/containers/Post').default
