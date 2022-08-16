0c4548c948db0becc56435f4c4a0ca7ca1efecaf
Carlos Alberto García de Alba Chávez <carlos.alberto.gdea.ch@gmail.com>
2022-08-16 11:13:13 -0500

    Changed all the Bootstrap elements to JSX-based components

--- src/components/Pages/FAQ/FAQ.jsx
+++ src/components/Pages/FAQ/FAQ.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 import Accordion from 'react-bootstrap/Accordion'
 
 import "./index.css"
@@ -9,62 +11,65 @@ function FAQ() {
   return (
     <>
       <section className='FAQSection section-container justify-content-center'>
-        <div class="row">
+        <Row>
           <h2 className='faq-title display-2'>Preguntas Frecuentes</h2>
-        </div>
+        </Row>
         <Accordion defaultActiveKey={['0']} alwaysOpen className='row accordion-flush'>
-          <div className='col-12 col-md-6 col-sm-12 col-xs-12'>
+
+          <Col xl="12" l="12" md="6" sm="12" xs="12">
             <Accordion.Item eventKey="0">
               <Accordion.Header >¿Lorem Ipsum dolor sit amet?</Accordion.Header>
               <Accordion.Body className="h3">
-                <div className='row'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vehicula lacinia odio. Etiam sem turpis, egestas ut eros commodo, laoreet porttitor velit.
-                </div>
-                <div className='row'>
+                <Row>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vehicula lacinia odio. Etiam sem turpis, egestas ut eros commodo, laoreet porttitor velit.
+                </Row>
+                <Row>
 
-                </div>
+                </Row>
               </Accordion.Body>
             </Accordion.Item>
             <Accordion.Item eventKey="1">
               <Accordion.Header>¿Lorem Ipsum dolor sit amet?</Accordion.Header>
               <Accordion.Body className="h3">
-                <div className='row'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vehicula lacinia odio. Etiam sem turpis, egestas ut eros commodo, laoreet porttitor velit.
-                </div>
+                <Row>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vehicula lacinia odio. Etiam sem turpis, egestas ut eros commodo, laoreet porttitor velit.
+                </Row>
 
-                <div className='row'>
+                <Row>
                   <button className="faq-button col-4">Lorem Ipsum</button>
-                </div>
+                </Row>
               </Accordion.Body>
             </Accordion.Item>
-          </div>
-          <div className='col-12 col-md-6 col-sm-12 col-xs-12'>
+          </Col>
+          <Col xl="12" l="12" md="6" sm="12" xs="12">
             <Accordion.Item eventKey="2">
               <Accordion.Header>¿Lorem Ipsum dolor sit amet</Accordion.Header>
               <Accordion.Body className="h3">
-                <div className='row'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vehicula lacinia odio. Etiam sem turpis, egestas ut eros commodo, laoreet porttitor velit.
-                </div>
-                
-                <div className='row mt-3'>
-                </div></Accordion.Body>
+                <Row>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vehicula lacinia odio. Etiam sem turpis, egestas ut eros commodo, laoreet porttitor velit.
+                </Row>
+
+                <Row className='mt-3'>
+                </Row>
+              </Accordion.Body>
 
             </Accordion.Item>
             <Accordion.Item eventKey="3">
               <Accordion.Header>¿Lorem Ipsum dolor sit amet</Accordion.Header>
               <Accordion.Body className="h3">
-              <div className='row'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vehicula lacinia odio. Etiam sem turpis, egestas ut eros commodo, laoreet porttitor velit.
-                </div>
-                
-                <div className="row mt-3">
+                <Row>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vehicula lacinia odio. Etiam sem turpis, egestas ut eros commodo, laoreet porttitor velit.
+                </Row>
+
+                <Row className="mt-3">
                   <a className="faq-link col-6">Lorem Ipsum</a>
-                </div></Accordion.Body>
+                </Row>
+              </Accordion.Body>
             </Accordion.Item>
-          </div>
+          </Col>
 
         </Accordion>
-        
+
       </section>
 
     </>
