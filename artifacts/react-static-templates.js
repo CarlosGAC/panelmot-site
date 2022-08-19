eca6a3882a97b491b3e1a1eed7c12d13555552ba
Daniel Ulises Velazquez Ponce <danicond97@gmail.com>
2022-08-19 18:34:08 -0500

    Revert "Ppvc 22/UI retouch"

--- artifacts/react-static-templates.js
+++ artifacts/react-static-templates.js
@@ -3,11 +3,12 @@
 
 // Template Map
 export default {
-  '__react_static_root__/node_modules/react-static/lib/browser/components/Default404': require('__react_static_root__/node_modules/react-static/lib/browser/components/Default404').default,
-'__react_static_root__/src/components/Pages/index.js': require('__react_static_root__/src/components/Pages/index.js').default,
-'__react_static_root__/src/components/Pages/NotFound/index.js': require('__react_static_root__/src/components/Pages/NotFound/index.js').default,
-'__react_static_root__/src/components/Pages/NotFound/NotFound.jsx': require('__react_static_root__/src/components/Pages/NotFound/NotFound.jsx').default
+  '__react_static_root__/src/pages/404.js': require('__react_static_root__/src/pages/404.js').default,
+'__react_static_root__/src/pages/about.js': require('__react_static_root__/src/pages/about.js').default,
+'__react_static_root__/src/pages/blog.js': require('__react_static_root__/src/pages/blog.js').default,
+'__react_static_root__/src/pages/index.js': require('__react_static_root__/src/pages/index.js').default,
+'__react_static_root__/src/containers/Post': require('__react_static_root__/src/containers/Post').default
 }
 
-export const notFoundTemplate = '__react_static_root__/node_modules/react-static/lib/browser/components/Default404'
+export const notFoundTemplate = '__react_static_root__/src/pages/404.js'
 
--- src/components/Atoms/FAQLink/FAQLink.styled.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const FAQLink = styled.a`
-font-weight: bold;
-color: black;
-text-decoration: underline;
-`;
